4fe7f35c7f862df7d1233efa247cc5bcf34d6cc6
Maciej Kurzak <kmk.kurzak@gmail.com>
2023-03-17 12:25:15 +0100

    add placeholder social image, fix seo

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from 'astro/config';
-import sanity from "astro-sanity";
+import sanity from 'astro-sanity';
 
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [sanity({
-    projectId: "n591yq65",
-    dataset: "production",
-    apiVersion: "2023-03-15",
-    useCdn: true
-  }), react()]
+    projectId: 'n591yq65',
+    dataset: 'production',
+    apiVersion: '2023-03-15',
+    useCdn: true,
+  }), react()],
+  site: 'http://localhost:3000',
 });
