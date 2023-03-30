14c7a0cbf2e901c4a2099df7e1db878dc8bd38d4
Maciej Kurzak <kmk.kurzak@gmail.com>
2023-03-30 23:03:09 +0200

    reenable astro-compress

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -2,20 +2,16 @@ import { defineConfig } from 'astro/config';
 import sanity from 'astro-sanity';
 import react from '@astrojs/react';
 import netlify from '@astrojs/netlify/functions';
-// import compress from "astro-compress";
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    sanity({
-      projectId: 'n591yq65',
-      dataset: 'production',
-      apiVersion: '2023-03-15',
-      useCdn: true,
-    }),
-    react(),
-    // compress()
-  ],
+  integrations: [sanity({
+    projectId: 'n591yq65',
+    dataset: 'production',
+    apiVersion: '2023-03-15',
+    useCdn: true,
+  }), react(), compress()],
   site: 'https://maciejkurzak.xyz',
   output: 'server',
   adapter: netlify(),
