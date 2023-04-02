a1a150e2698ccae8a354f87a4a74b6f6cd1f0a7e
Maciej Kurzak <kmk.kurzak@gmail.com>
2023-04-02 23:00:34 +0200

    change description

--- frontend/src/data/settings.ts
+++ frontend/src/data/settings.ts
@@ -1,6 +1,6 @@
 const settings = {
   title: "Maciej Kurzak - Portfolio",
-  description: "Junior Frontend Engineer with a passion for building web applications using modern technologies such as TypeScript and React that loves creativity and problem-solving involved in web development, constantly striving to improve skills and learn new things.",
+  description: "Junior Frontend Developer with a passion for building web applications using modern technologies such as TypeScript and React that loves creativity and problem-solving involved in web development, constantly striving to improve skills and learn new things.",
   url: "", // No trailing slash!
   // JSON LD
   name: "Example",
